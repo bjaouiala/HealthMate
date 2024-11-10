fbdfe44a2a62466cd72d03c6724efeed2b3d544a
bjaouiala <bjaouiala3@gmail.com>
2024-11-10 17:22:16 +0100

    upgrade commit

---
+++ package/src/app/pages/authentication/side-login/side-login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MaterialModule } from '../../../material.module';
+import { MatButtonModule } from '@angular/material/button';
+import {AuthRequest} from "../../../services/auth-service/model/authenticationModel/authRequest";
+
+@Component({
+  selector: 'app-side-login',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
+  templateUrl: './side-login.component.html',
+})
+export class AppSideLoginComponent {
+  authRequest:AuthRequest={email: "", password: ""}
+  constructor(private router: Router) { }
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    this.authRequest = this.form.value
+    this.router.navigate(['/']);
+  }
+}
