7ab7018f9dd79a5d8b32fbbeda1805eb580148d7
bjaouiala <bjaouiala3@gmail.com>
2024-11-11 13:28:14 +0100

    jwt commit

--- HealthMate/src/main/java/com/healthmate/healthmate/security/JwtFilter.java
+++ HealthMate/src/main/java/com/healthmate/healthmate/security/JwtFilter.java
@@ -21,7 +21,6 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-
     private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
